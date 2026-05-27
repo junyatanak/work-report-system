77f389a265d9bad86f16783788bf58863fbee5fd
junyatanak <piletree0418@gmail.com>
2026-05-27 07:12:59 +0700

    remove WorkerReport entity

--- Models/Process.cs
+++ Models/Process.cs
@@ -6,10 +6,6 @@ public class Process
 {
     public int Id { get; set; }
     public string Name { get; set; } = String.Empty;
-    public string Operation { get; set; } = String.Empty;
-    public int OutputPerCycle { get; set; }
-    public double TargetCycleTime { get; set; }
-
     public ICollection<WorkReport> WorkReports {get;} = new List<WorkReport>();
 
 }
