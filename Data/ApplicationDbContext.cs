fe9626fa5bae599548f94e325377c253e588c70b
junyatanak <piletree0418@gmail.com>
2026-06-02 21:01:08 +0700

    revise applicationdbcontext.cs

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,14 @@
 using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace DailyWorkReport.Data;
 
-public class ApplicationDbContext:IdentityDbContext<ApplicationUser>
+public class ApplicationDbContext:IdentityDbContext<IdentityUser>
 {
-    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
 
 }
