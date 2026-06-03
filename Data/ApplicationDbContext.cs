289be99fcd0ecc29577582a41a27a51588f44d31
junyatanak <piletree0418@gmail.com>
2026-06-03 20:56:47 +0700

    add applicationuser.cs

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,12 +1,11 @@
 using System;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using DailyWorkReport.Models;
 
 namespace DailyWorkReport.Data;
 
-public class ApplicationDbContext:IdentityDbContext<IdentityUser>
+public class ApplicationDbContext:IdentityDbContext<ApplicationUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace DailyWorkReport.Models;
+
+public class ApplicationUser : IdentityUser
+{
+}
