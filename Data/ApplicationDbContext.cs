47cfa23cc9335a1a54a146d674384c9e7b875a0d
junyatanak <piletree0418@gmail.com>
2026-06-05 07:05:53 +0700

    add dbset properties

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -21,5 +21,16 @@ public class ApplicationDbContext:IdentityDbContext<ApplicationUser>
                     });
     }
 
+    public DbSet<Process> Processes { get; set; } = null!;
+    public DbSet<ProcessWorkPattern> ProcessWorkPatterns { get; set; } = null!;
+    public DbSet<Product> Products { get; set; } = null!;
+    public DbSet<ProductionOrder> ProductionOrders { get; set; } = null!;
+    public DbSet<StandardWorkTime> StandardWorkTimes { get; set; } = null!;
+    public DbSet<WorkClass> WorkClasses { get; set; } = null!;
+    public DbSet<Worker> Workers { get; set; } = null!;
+    public DbSet<WorkPattern> WorkPatterns { get; set; } = null!;
+    public DbSet<WorkReport> WorkReports { get; set; } = null!;
+    public DbSet<WorkReportWorker> WorkReportWorkers { get; set; } = null!;
+
 
 }
