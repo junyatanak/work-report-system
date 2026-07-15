00ecdb54471a82efd81bac7663d12ba24883aa43
junyatanak <piletree0418@gmail.com>
2026-07-15 07:24:11 +0700

    add edit post action

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -94,10 +94,81 @@ namespace DailyWorkReport.Controllers
 
         }
 
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id)
+        {
+            if(string.IsNullOrEmpty(id)) return NotFound();
+
+            var user = await _userManager.FindByIdAsync(id);
+            if(user == null) return NotFound();
+            var roles = await _userManager.GetRolesAsync(user);
+            var vm = new UserDisplayViewModel
+            {
+                Id = user.Id,
+                UserName = user.UserName ?? string.Empty,
+                RoleName = roles.FirstOrDefault() ?? string.Empty
+            };
+            await PopulateRoleSelectList(vm.RoleName);
+            return View(vm);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, UserDisplayViewModel input)
+        {
+            if(string.IsNullOrEmpty(id) || id != input.Id) return NotFound();
+            if(!ModelState.IsValid)
+            {
+                await PopulateRoleSelectList(input.RoleName);
+                return View(input);
+            }
+            var user = await _userManager.FindByIdAsync(id);
+            if(user == null) return NotFound();
+            user.UserName = input.UserName;
+            var updateResult = await _userManager.UpdateAsync(user);
+            if(!updateResult.Succeeded)
+            {
+                foreach(var error in updateResult.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                await PopulateRoleSelectList(input.RoleName);
+                return View(input);
+            }
+            var currentRoles = await _userManager.GetRolesAsync(user);
+            if (!currentRoles.Contains(input.RoleName))
+            {
+                if (currentRoles.Any())
+                {
+                    var removeResult = await _userManager.RemoveFromRolesAsync(user, currentRoles);
+                    if (!removeResult.Succeeded)
+                    {
+                        foreach (var error in removeResult.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                        await PopulateRoleSelectList(input.RoleName);
+                        return View(input);
+                    }
+                }
+                var addResult = await _userManager.AddToRoleAsync(user, input.RoleName);
+                if (!addResult.Succeeded)
+                {
+                    foreach (var error in addResult.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    await PopulateRoleSelectList(input.RoleName);
+                    return View(input);
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
         private async Task PopulateRoleSelectList(string? selected = null)
         {
             var roles = await _roleManager.Roles.ToListAsync();
-            ViewData["RoleName"] = new SelectList(roles, "Name", "Name", selected);
+            ViewData["RoleName"] = new SelectList(roles, "Name", "Name", selected);            
         }
 
 
