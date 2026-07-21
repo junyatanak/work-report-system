818648289903fb9839ff7685574a7fbd3bc40fb7
junyatanak <piletree0418@gmail.com>
2026-07-21 07:41:56 +0700

    add content to sidemenu list

--- ViewComponents/SideMenuViewComponent.cs
+++ ViewComponents/SideMenuViewComponent.cs
@@ -11,6 +11,24 @@ public class SideMenuViewComponent : ViewComponent
         {
             new () {Text = "Work Report", Controller = "WorkReports", Action = "Index", IconClass = "bi-file-text"}
         };
+        if (User.IsInRole("Admin"))
+        {
+            menuItems.Add(new()
+            {
+                Text = "Master Data",
+                IconClass = "bi-database",
+                SubItems = new List<MenuItemViewModel>
+                {
+                    new() { Text = "Product", Controller = "Products", Action = "Index" },
+                    new() { Text = "Work Class", Controller = "WorkClasses", Action = "Index" },
+                    new() { Text = "Process", Controller = "Processes", Action = "Index" },
+                    new() { Text = "Work Pattern", Controller = "WorkPatterns", Action = "Index" },
+                    new() { Text = "Standard Work Time", Controller = "StandardWorkTimes", Action = "Index" },
+                    new() { Text = "Worker", Controller = "Workers", Action = "Index" },
+                }
+            });
+            menuItems.Add(new(){Text = "User", Controller = "Users", Action = "Index", IconClass = "bi-people"});
+        }
 
         return View(menuItems);
     }
