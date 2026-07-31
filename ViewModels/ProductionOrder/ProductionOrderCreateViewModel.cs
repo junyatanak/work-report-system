994da18894c5a87c41f0eafe742a8b1a895db533
junyatanak <piletree0418@gmail.com>
2026-07-31 07:12:53 +0700

    add createviewmodel

---
+++ ViewModels/ProductionOrder/ProductionOrderCreateViewModel.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DailyWorkReport.ViewModels.ProductionOrder;
+
+public class ProductionOrderCreateViewModel
+{
+    
+    [Required(ErrorMessage = "Order number is required.")]  
+    public string OrderNumber { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Product code is required.")]
+    public string ProductCode { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Please enter a valid product code.")]
+    public int? ProductId { get; set; }
+
+    [Required(ErrorMessage = "Order quantity is required.")]
+    [Range(1, int.MaxValue, ErrorMessage = "Order quantity must be at least 1.")]
+    public int OrderQty { get; set; }
+
+    [Required(ErrorMessage = "Due date is required.")]
+    public DateOnly DueDate { get; set; }
+}
